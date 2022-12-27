4ed0e9297a172a51c68a5c21448173a0b87f9c6c
sidoncode <siddharth54330@gmail.com>
2022-12-27 10:49:04 +0530

    inital Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  render() {
+      return(
+
+        <div>
+
+          <h1>This is the Day - 2 In Bennette University</h1>
+          
+          <h2>Reactjs || and JSX are - Main Agenda For Todays Session </h2>
+
+          <p>Happy Learning! :D </p>
+
+        </div>
+
+      );
+  }
+
+  }
+  
+export default App;
