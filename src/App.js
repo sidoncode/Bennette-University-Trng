c03adca9dc25fc2bf89c3d86bf3f3b5fe81f8843
sidoncode <siddharth54330@gmail.com>
2022-12-27 11:05:14 +0530

    jsx-styling

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import './App.css';
 class App extends Component {
   
   render() {
+
+    var mycustomStyle = {
+      fontsize:900,
+      fontfamily: 'Courier',
+      color: '#003300'
+    }
+  
       return(
-
-        <div>
-
+        
+        /*<div>
           <h1 className = "hello"> {20+25}</h1>
-          
           <h2>Reactjs || and JSX are - Main Agenda For Todays Session </h2>
-
           <p data-demoAttribute="demo">This is the Sample for JSX - Attribute</p>
-
+        </div>*/
+        <div>
+          <h1 style={mycustomStyle}>Hi This where the CustomStyle Tag Begins</h1>
         </div>
 
       );
